db78d474d0440f5f176a1c859ea6c12ff2ed7879
kuldeepmishra03642 <110096587+kuldeepmishra03642@users.noreply.github.com>
2022-09-11 08:45:19 -0700

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
 import React, { Component, useState } from "react";
 import '../styles/App.css';
+import '../components/Heading';
+import '../components/InputQuery';
+import '../components/SubHeading';
+import '../components/SubmitButton';
+import Heading from "../components/Heading";
+import InputQuery from "../components/InputQuery";
+import SubHeading from "../components/SubHeading";
+import SubmitButton from "../components/SubmitButton";
 
 const App = () => {
   return (
     <div id="main">
-      < />
-      < />
-      < />
-      < />
+      < Heading/>
+      < InputQuery/>
+      < SubHeading/>
+      < SubmitButton/>
     </div>
   )
 }
